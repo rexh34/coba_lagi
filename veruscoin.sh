c4f1b584541ba4542aea5cfc188d58d6ccc4d5c8
rexh34 <97252436+rexh34@users.noreply.github.com>
2022-01-06 19:14:47 +0000

    Add files via upload

---
+++ veruscoin.sh
@@ -0,0 +1,8 @@
+sudo su
+#
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz 
+#
+tar -xvf hellminer_cpu_linux.tar.gz 
+#
+./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RWTn9LeM4uhjvusCe6RFSueCu8VHEhXbFk.xTr -p hybrid --cpu 3
+#
